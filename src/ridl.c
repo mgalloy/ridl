b1f3965215305b7d2a1a4d8ca211d198301ff005
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2009-07-22 13:12:06 +0000

    Still working on exit/cleanup problem.

--- src/ridl.c
+++ src/ridl.c
@@ -3,29 +3,38 @@
 #include "ridl.h"
 
 
+void ridl_exit_handler(void) {  
+  printf("Bye!");
+}
+
+
 void ridl_welcome(void) {
   printf("rIDL 0.1: Really Interactive Data Language\n");  
 }
 
-void ridl_cleanup(void) {
-  IDL_Cleanup(IDL_FALSE);
+
+int ridl_cleanup(void) {
+  return(IDL_Cleanup(IDL_FALSE));
 }
+
 
 void ridl_print_prompt(void) {
   printf("rIDL> ");
 }
 
+
 int main(int argc, char **argv) {  
   IDL_INIT_DATA init_data;  
   char line[MAX_LINE_LENGTH];
-  int error;
+  int error, exit_value;
   
   init_data.options = IDL_INIT_CLARGS;  
   init_data.clargs.argc = argc;  
   init_data.clargs.argv = argv;  
+  ridl_welcome();
   
   if (IDL_Initialize(&init_data)) {
-    ridl_welcome();
+    IDL_ExitRegister(ridl_exit_handler);
     
     // loop while active
     ridl_print_prompt();
@@ -43,10 +52,11 @@ int main(int argc, char **argv) {
     error = IDL_ExecuteStr(line);
     printf("code = %d\n", error);
     
-    ridl_cleanup();
+    exit_value = ridl_cleanup();
   } else {
     printf("Failed to initialize Callable IDL session\n");
+    exit_value = 0;
   }
    
-  return 1;   
+  return exit_value;
 } 
