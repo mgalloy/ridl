85b6cb9c302644460ef9db438f0b86477f4659a4
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2009-07-22 22:33:32 +0000

    Working with signals.

--- src/ridl.c
+++ src/ridl.c
@@ -1,10 +1,27 @@
 #include <stdio.h>   
+#include <signal.h>
 #include "idl_export.h" 
 #include "ridl.h"
 
+static int exit_value;
+
+static IDL_MSG_DEF msg_arr[] = {  
+#define M_RIDL_SIGNAL_REG       0
+  {  "M_RIDL_SIGNAL_REG",   "%NSignal registration problem." }, 
+};
+static IDL_MSG_BLOCK msg_block; 
+
+
+void ridl_signal_handler(int signo) {
+  printf("Received signal %d\n", signo);
+  
+  exit_value = IDL_Cleanup(IDL_FALSE);
+  exit(exit_value);
+}
+
 
 void ridl_exit_handler(void) {  
-  printf("Bye!");
+  printf("Bye!\n");
 }
 
 
@@ -13,48 +30,50 @@ void ridl_welcome(void) {
 }
 
 
-int ridl_cleanup(void) {
-  return(IDL_Cleanup(IDL_FALSE));
-}
-
-
 void ridl_print_prompt(void) {
   printf("rIDL> ");
 }
 
 
-int main(int argc, char **argv) {  
+int main(int argc, char *argv[]) {  
   IDL_INIT_DATA init_data;  
   char line[MAX_LINE_LENGTH];
-  int error, exit_value;
+  int error;
+  IDL_SignalSet_t *set;
   
+  // TODO: these command line args don't seem to be used
   init_data.options = IDL_INIT_CLARGS;  
   init_data.clargs.argc = argc;  
   init_data.clargs.argv = argv;  
-  ridl_welcome();
   
+  ridl_welcome();
+                                               
   if (IDL_Initialize(&init_data)) {
     IDL_ExitRegister(ridl_exit_handler);
-    
-    // loop while active
-    ridl_print_prompt();
-    fgets(line, MAX_LINE_LENGTH, stdin);
-    error = IDL_ExecuteStr(line);
-    printf("code = %d\n", error);
-    
-    ridl_print_prompt();
-    fgets(line, MAX_LINE_LENGTH, stdin);
-    error = IDL_ExecuteStr(line);
-    printf("code = %d\n", error);
 
-    ridl_print_prompt();
-    fgets(line, MAX_LINE_LENGTH, stdin);
-    error = IDL_ExecuteStr(line);
-    printf("code = %d\n", error);
+    if (!(msg_block = IDL_MessageDefineBlock("RIDL_MSG_BLOCK", 
+                                             IDL_CARRAY_ELTS(msg_arr),  
+                                             msg_arr))) return IDL_FALSE; 
+                                           
+    // TODO: right now you have to cntrl-C to exit ridl
+    //IDL_SignalRegister(SIGINT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+    //IDL_SignalRegister(SIGABRT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+    //IDL_SignalRegister(SIGKILL, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+    //IDL_SignalRegister(SIGQUIT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+    //IDL_SignalRegister(SIGTERM, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+    //IDL_SignalRegister(SIGSTOP, ridl_signal_handler, M_RIDL_SIGNAL_REG);
     
-    exit_value = ridl_cleanup();
+    while (1) {
+      ridl_print_prompt();
+      if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL) { 
+        printf("\n");
+        exit_value = IDL_Cleanup(IDL_FALSE); 
+      };
+      error = IDL_ExecuteStr(line);
+      //printf("code = %d\n", error);
+    }    
   } else {
-    printf("Failed to initialize Callable IDL session\n");
+    printf("Failed to initialize Callable IDL session.\n");
     exit_value = 0;
   }
    
