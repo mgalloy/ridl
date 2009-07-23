4cd95277d7e47a840e31de02fc43b4443f700146
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2009-07-23 19:37:20 +0000

    .edit works with mate -w.

--- src/ridl.c
+++ src/ridl.c
@@ -1,9 +1,13 @@
 #include <stdio.h>  
 #include <stdlib.h> 
 #include <signal.h>
+
 #include "idl_export.h" 
 #include "readline/readline.h"
 #include "ridl.h"
+
+static char *ridl_version = "0.1";
+static int show_welcome = 1;
 
 static IDL_MSG_DEF msg_arr[] = {  
 #define M_RIDL_SIGNAL_REG       0
@@ -18,7 +22,9 @@ void ridl_exit_handler(void) {
 
 
 void ridl_welcome(void) {
-  printf("rIDL 0.1: Really Interactive Data Language\n");  
+  if (show_welcome) {
+    printf("rIDL 0.1: Really Interactive Data Language\n");  
+  }
 }
 
 
@@ -47,25 +53,91 @@ char *ridl_getnextword(char *line, int start) {
 }
 
 
+char *ridl_findpath(char *filename) {
+  // TODO: find full path for filename referred to on .edit line
+  char *fullpath = (char *)malloc(strlen(filename) + 1);
+  strcpy(fullpath, filename);
+}
+
+
+void ridl_launcheditor(char *filename) {
+  char *editor = "mate -w";    // TODO: change editor to use $EDITOR
+  char *cmd = (char *)malloc(strlen(filename) + strlen(editor) + 1 + 1);
+  sprintf(cmd, "%s %s", editor, filename);
+  system(cmd);
+  free(cmd);
+}
+
+
 void ridl_printversion(void) {
-  // TODO: put version info in here
-  printf("showing version\n");
-  exit(EXIT_SUCCESS);  
+  IDL_STRING *version = IDL_SysvVersionRelease();
+  IDL_STRING *os = IDL_SysvVersionOS();
+  IDL_STRING *arch = IDL_SysvVersionArch();
+  printf("rIDL %s: IDL %s %s %s\n", 
+         ridl_version, 
+         IDL_STRING_STR(version), 
+         IDL_STRING_STR(os), 
+         IDL_STRING_STR(arch));
 }
 
 
 void ridl_printhelp(void) {
-  // TODO: put help in here
-  printf("show help...\n");
-  exit(EXIT_SUCCESS);
+  char *indent = "  ";
+  int switch_width = 25;
+  ridl_printversion();
+  
+  printf("usage: ridl [options]\n");
+  printf("\n");
+  printf("options:\n");
+  printf("%s%-*s %s\n", indent, switch_width, "-32", 
+         "start in 32-bit mode");
+  printf("%s%-*s %s\n", indent, switch_width, "-arg value", 
+         "saves a single command line argument");
+  printf("%s%-*s %s\n", indent, switch_width, "-args value1 value2 ...", 
+         "saves multiple command line arguments");
+  printf("%s%-*s %s\n", indent, switch_width, "-demo", 
+         "start in 7-minute demo mode");
+  printf("%s%-*s %s\n", indent, switch_width, "-e command", 
+         "executes command and exit");
+  printf("%s%-*s %s\n", indent, switch_width, "-em=FILENAME", 
+    "execute the given .sav file containing an embedded license");
+  printf("%s%-*s %s\n", indent, switch_width, "-h", 
+         "show this help message");
+  printf("%s%-*s %s\n", indent, switch_width, "-novm", 
+         "use 7-minute demo mode instead of virtual machine with -rt");
+  printf("%s%-*s %s\n", indent, switch_width, "-pref=FILENAME", 
+         "load the given preference file");
+  printf("%s%-*s %s\n", indent, switch_width, "-queue", 
+         "wait for a license when none are available");
+  printf("%s%-*s %s\n", indent, switch_width, "-quiet", 
+         "suppress printing information about IDL on startup");
+  printf("%s%-*s %s\n", indent, switch_width, "-rt=FILENAME", 
+         "start with a runtime license on the given .sav file");
+  printf("%s%-*s %s\n", indent, switch_width, "-student", 
+         "start in student mode");
+  printf("%s%-*s %s\n", indent, switch_width, "-ulicense", 
+         "start IDL with a unique license");
+  printf("%s%-*s %s\n", indent, switch_width, "-v", 
+         "print version information");
+  printf("%s%-*s %s\n", indent, switch_width, "-vm=FILENAME", 
+         "start the virtual machine with the given .sav file");
 }
 
 
 int main(int argc, char *argv[]) {  
   int a;
   for (a = 0; a < argc; a++) {
-    if (strcmp(argv[a], "-v") == 0) ridl_printversion();
-    if (strcmp(argv[a], "-h") == 0) ridl_printhelp();
+    if (strcmp(argv[a], "-v") == 0) { 
+      ridl_printversion();
+      exit(EXIT_SUCCESS);
+    }
+    if (strcmp(argv[a], "-h") == 0) {
+      ridl_printhelp();
+      exit(EXIT_SUCCESS);
+    }
+    if (strcmp(argv[a], "-quiet") == 0) {
+      show_welcome = 0;
+    }    
   }
   
   // TODO: these command line args don't seem to be used; -32 should not be
@@ -114,13 +186,11 @@ int main(int argc, char *argv[]) {
           if (firstchar == '.') {
             char *cmd = ridl_getnextword(line, firstcharIndex);
             if (strcmp(cmd, ".edit") == 0) {
-              // TODO: launch editor
-              //    1. find file in !path
-              //    2. lookup $EDITOR
-              //    3. spawn editor on full filename
               char *file = ridl_getnextword(line, firstcharIndex + strlen(cmd) + 1);
-              printf("launch EDITOR on %s.pro\n", file);
+              char *fullpath = ridl_findpath(file);
+              ridl_launcheditor(fullpath);
               free(file);
+              free(fullpath);
             } else {
               int error = IDL_ExecuteStr(line);
             }
