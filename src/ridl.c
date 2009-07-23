e630c60c06e5f311dad331cd22a6fb98e41898ee
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2009-07-23 06:08:14 +0000

    Checking for .edit and magic commands; starting to parse strings.

--- src/ridl.c
+++ src/ridl.c
@@ -1,4 +1,5 @@
-#include <stdio.h>   
+#include <stdio.h>  
+#include <stdlib.h> 
 #include <signal.h>
 #include "idl_export.h" 
 #include "readline/readline.h"
@@ -21,12 +22,39 @@ void ridl_welcome(void) {
 }
 
 
+int ridl_firstchar(char *line) {
+  int i;
+  for (i = 0; i < strlen(line); i++) {
+    if (line[i] != ' ') return(i);
+  }
+}
+
+
+char *ridl_getnextword(char *line, int start) {
+  int i;
+  for (i = start; i < strlen(line); i++) {
+    if (line[i] == ' ') break;
+  }
+  
+  int end = i;
+  
+  char *result = (char *)calloc(i - start + 1, 1);
+  for (i = start; i < end; i++) {
+    result[i - start] = line[i];
+  }
+  
+  return(result);
+}
+
+
 int main(int argc, char *argv[]) {  
   // TODO: these command line args don't seem to be used
   IDL_INIT_DATA init_data;    
   init_data.options = IDL_INIT_CLARGS;  
   init_data.clargs.argc = argc;  
   init_data.clargs.argv = argv;  
+  
+  // TODO: handle -h and -v also
   
   ridl_welcome();
                                                
@@ -38,7 +66,7 @@ int main(int argc, char *argv[]) {
                                              msg_arr))) return(1);                                            
     
     // TODO: add previous history to readline history
-    // TODO: add completer for routines
+    // TODO: add completer for routines and variables
     
     while (1) {
       char *line = readline ("rIDL> ");
@@ -50,12 +78,35 @@ int main(int argc, char *argv[]) {
       };
       
       // magic lines start with :, anything else is passed to IDL
-      if (line[0] == ':') {
+      int firstcharIndex = ridl_firstchar(line);
+      char firstchar = line[firstcharIndex];
+      if (firstchar == ':') {
         // TODO: handle magic lines
+        char *cmd = ridl_getnextword(line, firstcharIndex);
+        printf("magic command '%s'\n", cmd);
+        free(cmd);
       } else {
         if (line && *line) { 
           add_history(line);
-          int error = IDL_ExecuteStr(line);
+          
+          // check for .edit
+          if (firstchar == '.') {
+            char *cmd = ridl_getnextword(line, firstcharIndex);
+            if (strcmp(cmd, ".edit") == 0) {
+              // TODO: launch editor
+              //    1. find file in !path
+              //    2. lookup $EDITOR
+              //    3. spawn editor on full filename
+              char *file = ridl_getnextword(line, firstcharIndex + strlen(cmd) + 1);
+              printf("launch EDITOR on %s.pro\n", file);
+              free(file);
+            } else {
+              int error = IDL_ExecuteStr(line);
+            }
+            free(cmd);
+          } else {
+            int error = IDL_ExecuteStr(line);
+          }
         }
       }
       
