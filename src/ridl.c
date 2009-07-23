f57c42312e029ffab08c4a6730d9c12ace01c018
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2009-07-23 00:07:43 +0000

    Cleaning up readline use; adding stub for magic lines.

--- src/ridl.c
+++ src/ridl.c
@@ -4,8 +4,6 @@
 #include "readline/readline.h"
 #include "ridl.h"
 
-static int exit_value;
-
 static IDL_MSG_DEF msg_arr[] = {  
 #define M_RIDL_SIGNAL_REG       0
   {  "M_RIDL_SIGNAL_REG",   "%NSignal registration problem." }, 
@@ -13,16 +11,8 @@ static IDL_MSG_DEF msg_arr[] = {
 static IDL_MSG_BLOCK msg_block; 
 
 
-void ridl_signal_handler(int signo) {
-  //printf("Received signal %d\n", signo);
-  
-  exit_value = IDL_Cleanup(IDL_FALSE);  
-}
-
-
 void ridl_exit_handler(void) {  
-  printf("Bye!\n");
-  //exit(exit_value);
+  //printf("Bye!\n");
 }
 
 
@@ -31,18 +21,9 @@ void ridl_welcome(void) {
 }
 
 
-void ridl_print_prompt(void) {
-  printf("rIDL> ");
-}
-
-
 int main(int argc, char *argv[]) {  
-  IDL_INIT_DATA init_data;  
-  char *line;
-  int error;
-  IDL_SignalSet_t *set;
-  
   // TODO: these command line args don't seem to be used
+  IDL_INIT_DATA init_data;    
   init_data.options = IDL_INIT_CLARGS;  
   init_data.clargs.argc = argc;  
   init_data.clargs.argv = argv;  
@@ -54,34 +35,37 @@ int main(int argc, char *argv[]) {
 
     if (!(msg_block = IDL_MessageDefineBlock("RIDL_MSG_BLOCK", 
                                              IDL_CARRAY_ELTS(msg_arr),  
-                                             msg_arr))) return IDL_FALSE; 
-                                           
-    // TODO: right now you have to cntrl-C to exit ridl
-    //IDL_SignalRegister(SIGINT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
-    //IDL_SignalRegister(SIGABRT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
-    //IDL_SignalRegister(SIGKILL, ridl_signal_handler, M_RIDL_SIGNAL_REG);
-    //IDL_SignalRegister(SIGQUIT, ridl_signal_handler, M_RIDL_SIGNAL_REG);
-    //IDL_SignalRegister(SIGTERM, ridl_signal_handler, M_RIDL_SIGNAL_REG);
-    //IDL_SignalRegister(SIGSTOP, ridl_signal_handler, M_RIDL_SIGNAL_REG);
+                                             msg_arr))) return(1);                                            
+    
+    // TODO: add previous history to readline history
+    // TODO: add completer for routines
     
     while (1) {
       char *line = readline ("rIDL> ");
+      
+      // normal exit by hitting ^D
       if (line == NULL) { 
         printf("\n");
-        exit_value = IDL_Cleanup(IDL_FALSE); 
+        return(IDL_Cleanup(IDL_FALSE)); 
       };
       
-      // TODO: check for "magic" line that begins with ":"
+      // magic lines start with :, anything else is passed to IDL
+      if (line[0] == ':') {
+        // TODO: handle magic lines
+      } else {
+        if (line && *line) { 
+          add_history(line);
+          int error = IDL_ExecuteStr(line);
+        }
+      }
       
-      add_history(line);
-      error = IDL_ExecuteStr(line);
       free(line);
-      //printf("code = %d\n", error);
     }    
   } else {
     printf("Failed to initialize Callable IDL session.\n");
-    exit_value = 1;
+    return(1);
   }
-   
-  return(exit_value);
+  
+  // TODO: exit values don't seem to be passed to shell
+  return(1);
 } 
