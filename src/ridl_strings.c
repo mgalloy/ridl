be50e0672c27f925a0e4e231fef714d82d902301
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2010-03-07 05:24:43 +0000

    Docs.

--- src/ridl_strings.c
+++ src/ridl_strings.c
@@ -3,16 +3,28 @@
 #include <string.h>
 
 
-/*
-  char *s = (char *)malloc(1024);
-  char *t = (char *)malloc(1024);
-  strcpy(t, "Hello my name is %name and I live in %city!\n");
-  ridl_replacestr(s, t, "name", "Mike");
-  printf("%s", s);
- 
-  strcpy(t, s);
-  ridl_replacestr(s, t, "city", "Boulder");
-  printf("%s", s);
+/**
+   Replace `name` with `value` in `text` and return the result in `result`.
+
+   Use it as in the following example:
+   @code
+     char *s = (char *)malloc(1024);
+     char *t = (char *)malloc(1024);
+     strcpy(t, "Hello my name is %name and I live in %city!\n");
+     ridl_replacestr(s, t, "name", "Mike");
+     printf("%s", s);
+  
+     strcpy(t, s);
+     ridl_replacestr(s, t, "city", "Boulder");
+     printf("%s", s);
+   @endcode
+   
+   @return 1 if matched, 0 if not
+   
+   @param[out] result string to return result in
+   @param[in] text string to look for `name` in
+   @param[in] name string in `text` to replace
+   @param[in] value string to replace `name` with
 */
 int ridl_replacestr(char *result, char *text, char *name, char *value) {
   char *pos, *original_name, *original_value;
