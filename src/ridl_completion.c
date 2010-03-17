cf480e921bfeb1d7b6589217ba26333869052ff4
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2010-03-17 23:19:33 +0000

    Does completion of local variables (except that it should uppercase typed value).

--- src/ridl_completion.c
+++ src/ridl_completion.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> 
 #include <string.h>
 
+#include "idl_export.h" 
 #include "readline/readline.h"
 
 #include "ridl_strings.h"
@@ -15,6 +16,8 @@ char *system_variables[] = {
   "!quiet", "!radeg", "!values", "!version", "!warn", "!x", "!y", "!z",
   (char *)NULL
 };
+
+IDL_VPTR local_variables;
 
 
 /* Look up NAME as the name of a command, and return a pointer to that 
@@ -30,6 +33,48 @@ char **ridl_find_systemvariable(char *name) {
   }
   
   return ((char **)NULL);
+}
+
+
+void ridl_get_localvariables_list(void) {
+  int status = IDL_ExecuteStr("_ridl_localvars = scope_varname()");
+   local_variables = IDL_FindNamedVariable("_ridl_localvars", 0);
+}
+
+
+void ridl_remove_localvariables_list(void) {
+  int status = IDL_ExecuteStr("delvar, _ridl_localvars");
+}
+
+char *ridl_localvariable_generator(const char *text, int state) {
+  static int list_index, len;  
+  int nlocals = (int) (*local_variables->value.arr).n_elts;
+  int i;
+  IDL_STRING *s = (IDL_STRING *)local_variables->value.arr->data;
+  char *name;
+  
+  // state == 0 the first time this is called, non-zero on subsequent calls
+  if (!state) {
+    list_index = 0;
+    len = strlen(text);
+  }
+    
+  //printf("\nnumber of local variables = %d\n", nlocals);
+  
+  //for (i = 0; i < nlocals; i++) {
+  //  printf("  %s\n", IDL_STRING_STR(&s[i]));
+  //}
+
+  while (list_index < nlocals) {
+    name = IDL_STRING_STR(&s[list_index]);
+    //printf("checking against %s...\n", name);
+    list_index++;
+    if (strncmp(name, text, len) == 0) {
+      return(ridl_copystr(name));
+    }
+  }
+    
+  return((char *)NULL);
 }
 
 
@@ -69,10 +114,25 @@ char **ridl_completion(const char *text, int start, int end) {
   int loc;
   
   //printf("\ntext = %s, start = %d, end = %d\n", text, start, end);
+
+  rl_completion_append_character = '\0';
   
   // check for system variables first
-  rl_completion_append_character = '\0';
   matches = rl_completion_matches(text, ridl_systemvariable_generator);
+
+  // check for local variables if no system variables found
+  if (matches == (char **)NULL) {
+    ridl_get_localvariables_list();
+    matches = rl_completion_matches(text, ridl_localvariable_generator);
+    ridl_remove_localvariables_list();
+  }
+  
+  // TODO: check for structure fields
+  
+  // TODO: check for routine names if nothing found yet
+  if (matches == (char **)NULL) {
+    
+  }
   
   return(matches);
 }
