e6ba4010e9329b1344c63343f34587d95c079005
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2011-05-25 21:23:08 +0000

    Checking for Readline version 6.0 now, though not entirely sure what is requireed (6.1 works, 5.0 does not).

--- CMake/Modules/FindReadline.cmake
+++ CMake/Modules/FindReadline.cmake
@@ -5,7 +5,31 @@ if (Readline_INCLUDE_DIR AND Readline_LIBRARY)
   set (Readline_FOUND TRUE)
 endif ()
 
+if (Readline_INCLUDE_DIR AND EXISTS "${Readline_INCLUDE_DIR}/readline/readline.h")
+  file(STRINGS "${Readline_INCLUDE_DIR}/readline/readline.h" 
+               Readline_MAJOR_VERSION 
+       REGEX "^#define RL_VERSION_MAJOR.*[0-9]+$")
+  file(STRINGS "${Readline_INCLUDE_DIR}/readline/readline.h" 
+               Readline_MINOR_VERSION 
+       REGEX "^#define RL_VERSION_MINOR.*[0-9]+$")
+
+  string(REGEX REPLACE "^.*RL_VERSION_MAJOR.*([0-9]+).*$" 
+                       "\\1" 
+                       Readline_MAJOR_VERSION 
+                       "${Readline_MAJOR_VERSION}")
+  string(REGEX REPLACE "^.*RL_VERSION_MINOR.*([0-9]+).*$" 
+                       "\\1" 
+                       Readline_MINOR_VERSION 
+                       "${Readline_MINOR_VERSION}")
+
+  set(Readline_VERSION "${Readline_MAJOR_VERSION}.${Readline_MINOR_VERSION}")
+endif ()
+
+
 if (Readline_FOUND)
+  if (Readline_FIND_VERSION VERSION_GREATER Readline_VERSION)
+    message(FATAL_ERROR "Found version ${Readline_VERSION} of GNU Readline at ${Readline_LIBRARY}, but version ${Readline_FIND_VERSION} required")
+  endif ()
   if (NOT Readline_FIND_QUIETLY)
     message(STATUS "Found GNU Readline: ${Readline_LIBRARY}")
   endif ()
