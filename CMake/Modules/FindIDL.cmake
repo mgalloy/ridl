2ac0bf55b382a32cf857ed0421eb6e2ea5f5265f
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2011-07-13 02:22:24 +0000

    Passing 4 or 8 as a component to FindIDL.cmake module to determine whether a 32-bit or 64-bit build is needed.

--- CMake/Modules/FindIDL.cmake
+++ CMake/Modules/FindIDL.cmake
@@ -13,10 +13,11 @@ elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
   set(_IDL_OS ".linux")
 endif ()
 
+
 # find IDL based on version numbers; if you want a specific one, set
 # it prior to running configure
 if (NOT DEFINED IDL_FIND_VERSION)
-  set(_IDL_KNOWN_VERSIONS "81" "80" "71" "706")
+  set(_IDL_KNOWN_VERSIONS "81" "80" "71" "706" "64")
 
   set(_IDL_SEARCH_DIRS "${_IDL_PROGRAM_FILES_DIR}")
   foreach (_IDL_KNOWN_VERSION ${_IDL_KNOWN_VERSIONS})
@@ -38,11 +39,20 @@ find_path(IDL_INCLUDE_DIR
   PATH_SUFFIXES external/include
   )
 
+if (IDL_FIND_COMPONENTS EQUAL 8)
+  set(_IDL_BIN_SUFFIXES /bin/bin${_IDL_OS}.x86_64)
+elseif (IDL_FIND_COMPONENTS EQUAL 4)
+  set(_IDL_BIN_SUFFIXES /bin/bin${_IDL_OS}.x86 /bin/bin${_IDL_OS}.i386)
+else ()
+  math(EXPR IDL_FIND_COMPONENTS_BITS "8 * ${IDL_FIND_COMPONENTS}") 
+  message(FATAL_ERROR "${IDL_FIND_COMPONENTS_BITS}-bit build invalid")
+endif ()
+
 find_library(IDL_LIBRARY
   NAMES idl
   PATHS ${_IDL_SEARCH_DIRS}
   HINTS ${IDL_ROOT}
-  PATH_SUFFIXES /bin/bin${_IDL_OS}.x86_64 /bin/bin${_IDL_OS}.x86 /bin/bin${_IDL_OS}.i386
+  PATH_SUFFIXES ${_IDL_BIN_SUFFIXES}
   )
 
 if (IDL_INCLUDE_DIR AND IDL_LIBRARY)
