1d5882f2d1bb468ea99b09dd5f7651ab4467c660
mgalloy <mgalloy@6cd51d45-9568-0410-80bb-ac2a7bf28483>
2012-09-05 16:39:26 +0000

    Updating FindIDL for IDL 8.2.

--- CMake/Modules/FindIDL.cmake
+++ CMake/Modules/FindIDL.cmake
@@ -44,46 +44,56 @@
 #
 #        /usr/local/itt/idl/idl80
 
-# convenience variable for IDL's default install directories
+# convenience variable for ITT's install dir
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
-  set(_IDL_PROGRAM_FILES_DIR "C:/Program Files/ITT/IDL")
+  set(_IDL_PROGRAM_FILES_DIR "C:/Program Files")
   set(_IDL_NAME "IDL")
   set(_IDL_OS "")
+  set(_IDL_KNOWN_COMPANIES "Exelis" "ITT")
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-  set(_IDL_PROGRAM_FILES_DIR "/Applications/itt/idl")
+  set(_IDL_PROGRAM_FILES_DIR "/Applications")
   set(_IDL_NAME "idl")
   set(_IDL_OS ".darwin")
+  set(_IDL_KNOWN_COMPANIES "exelis" "itt")
 elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  set(_IDL_PROGRAM_FILES_DIR "/usr/local/itt/idl")
+  set(_IDL_PROGRAM_FILES_DIR "/usr/local")
   set(_IDL_NAME "idl")
   set(_IDL_OS ".linux")
+  set(_IDL_KNOWN_COMPANIES "exelis" "itt")
 endif ()
 
 
-# find IDL based on version numbers; if you want a specific one, set
+# find idl based on version numbers, if you want a specific one, set
 # it prior to running configure
 if (NOT DEFINED IDL_FIND_VERSION)
-  set(_IDL_KNOWN_VERSIONS "81" "80" "71" "706" "64")
-
-  set(_IDL_SEARCH_DIRS "${_IDL_PROGRAM_FILES_DIR}")
-  foreach (_IDL_KNOWN_VERSION ${_IDL_KNOWN_VERSIONS})
-    list(APPEND _IDL_SEARCH_DIRS
-         "${_IDL_PROGRAM_FILES_DIR}/${_IDL_NAME}${_IDL_KNOWN_VERSION}")
-  endforeach ()
-  
-  set(APPEND _IDL_SEARCH_DIRS "${_IDL_PROGRAM_FILES_DIR}/${_IDL_NAME}")
-  foreach (_IDL_KNOWN_VERSION ${_IDL_KNOWN_VERSIONS})
-    list(APPEND _IDL_SEARCH_DIRS
-         "${_IDL_PROGRAM_FILES_DIR}${_IDL_KNOWN_VERSION}")
-  endforeach ()
+  set(_IDL_KNOWN_VERSIONS "82" "81" "80" "71" "706")
+# IDL 8.0 is in a different location than other versions on Windows (extra IDL directory in path)
+  foreach (_IDL_COMPANY ${_IDL_KNOWN_COMPANIES})
+    list(APPEND 
+         _IDL_SEARCH_DIRS 
+         "${_IDL_PROGRAM_FILES_DIR}/${_IDL_COMPANY}/${_IDL_NAME}/${_IDL_NAME}80")
+    list(APPEND 
+         _IDL_SEARCH_DIRS 
+         "${_IDL_PROGRAM_FILES_DIR}/${_IDL_COMPANY}/${_IDL_NAME}/${_IDL_NAME}81")
+    foreach (_IDL_KNOWN_VERSION ${_IDL_KNOWN_VERSIONS})
+      list(APPEND _IDL_SEARCH_DIRS
+           "${_IDL_PROGRAM_FILES_DIR}/${_IDL_COMPANY}/${_IDL_NAME}${_IDL_KNOWN_VERSION}")
+    endforeach (_IDL_KNOWN_VERSION ${_IDL_KNOWN_VERSIONS})
+  endforeach (_IDL_COMPANY ${_IDL_KNOWN_COMPANIES})
 endif ()
+
+if (NOT "$ENV{IDL_DIR}" STREQUAL "")
+  set(_IDL_SEARCH_DIRS "$ENV{IDL_DIR}")
+endif ()
+
 
 find_path(IDL_INCLUDE_DIR
   idl_export.h
   PATHS ${_IDL_SEARCH_DIRS}
   HINTS ${IDL_ROOT}
   PATH_SUFFIXES external/include
-  )
+)
+
 
 set(IDL_BIN64_PATH /bin/bin${_IDL_OS}.x86_64)
   if (APPLE)
@@ -124,7 +134,7 @@ if (IDL_FOUND)
   get_filename_component(IDL_DIR ${_IDL_BIN_DIR} PATH)
 else ()
    if (IDL_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find IDL")
+      message(FATAL_ERROR "Could not find IDL. Use -DIDL_INCLUDE_DIR and -DIDL_LIBRARY to specify IDL location.")
    endif ()
 endif ()
 
