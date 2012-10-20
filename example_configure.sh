73dbc710c07ff2a4a4a865af084312596923fb81
Michael Galloy <mgalloy@txcorp.com>
2012-10-19 19:27:23 -0600

    Getting ignores set.

--- example_configure.sh
+++ example_configure.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cmake -DReadline_INCLUDE_DIR=/usr/local/Cellar/readline/6.2.1/include \
       -DReadline_LIBRARY=/usr/local/Cellar/readline/6.2.1/lib/libreadline.dylib \
       -DCMAKE_INSTALL_PREFIX=install \
