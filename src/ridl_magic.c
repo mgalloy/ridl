e4a7a2fb95cc5e2f8cf1122df265293020f9f55f
Michael Galloy <mgalloy@gmail.com>
2014-04-03 13:09:05 -0600

    Whitespace.

--- src/ridl_magic.c
+++ src/ridl_magic.c
@@ -42,9 +42,9 @@ int ridl_magic_help(char *line, int firstcharIndex) {
   char first_char[2];
   int plain_help = 1;
   int i;
-  
+
   first_char[1] = '\0';
-  
+
   if (strlen(argument) > 0) {
     if (strcmp(argument, "verbose") == 0) {
       sprintf(show_cmd, "$more %s/share/ridl_verbosehelp.txt", RIDL_DIR);
@@ -57,7 +57,7 @@ int ridl_magic_help(char *line, int firstcharIndex) {
       }
       sprintf(help_filename, "%s/online_help/%s/%s.txt", RIDL_DIR, first_char, argument);
       if (!ridl_file_exists(help_filename)) {
-        ridl_warning("unknown :help topic '%s'", argument); 
+        ridl_warning("unknown :help topic '%s'", argument);
       } else {
         sprintf(show_cmd, "$more %s", help_filename);
         result = IDL_ExecuteStr(show_cmd);
@@ -65,45 +65,45 @@ int ridl_magic_help(char *line, int firstcharIndex) {
     }
   } else {
     ridl_printversion();
-  
+
     printf("\nmagic commands:\n");
 
-    printf(magic_format, indent, magic_width, ":colors", 
-           "toggle whether color is used");  
-    printf(magic_format, indent, magic_width, ":doc routine", 
+    printf(magic_format, indent, magic_width, ":colors",
+           "toggle whether color is used");
+    printf(magic_format, indent, magic_width, ":doc routine",
            "show calling syntax and comment header for the routine");
-    printf(magic_format, indent, magic_width, ":help [cmd]", 
+    printf(magic_format, indent, magic_width, ":help [cmd]",
            "show this help message; show help on 'cmd', if present");
-    printf(magic_format, indent, magic_width, ":history [n] [nonum]", 
+    printf(magic_format, indent, magic_width, ":history [n] [nonum]",
            "show last n commands (defaults to 10); 'nonum' option hides command numbers");
-    printf(magic_format, indent, magic_width, ":histedit n filename", 
+    printf(magic_format, indent, magic_width, ":histedit n filename",
            "send last n commands to filename and launch editor");
-    printf(magic_format, indent, magic_width, ":log filename", 
+    printf(magic_format, indent, magic_width, ":log filename",
            "start logging all commands and output to filename");
-    printf(magic_format, indent, magic_width, ":unlog", 
+    printf(magic_format, indent, magic_width, ":unlog",
            "stop logging commands and output");
-    printf(magic_format, indent, magic_width, ":notebook filename", 
+    printf(magic_format, indent, magic_width, ":notebook filename",
            "start sending all commands and output to an HTML notebook filename");
-    printf(magic_format, indent, magic_width, ":unnotebook", 
+    printf(magic_format, indent, magic_width, ":unnotebook",
            "stop logging commands and output to notebook");
-    printf(magic_format, indent, magic_width, ":pref [NAME=value]", 
+    printf(magic_format, indent, magic_width, ":pref [NAME=value]",
            "set rIDL preference (or list all rIDL preferences) for the current session");
-    printf(magic_format, indent, magic_width, ":save_graphic", 
-           "save current new graphic, iTool, or direct graphic window to notebook");           
-    printf(magic_format, indent, magic_width, ":tee filename", 
+    printf(magic_format, indent, magic_width, ":save_graphic",
+           "save current new graphic, iTool, or direct graphic window to notebook");
+    printf(magic_format, indent, magic_width, ":tee filename",
            "start logging output to filename");
-    printf(magic_format, indent, magic_width, ":untee", 
+    printf(magic_format, indent, magic_width, ":untee",
            "stop logging output");
-    printf(magic_format, indent, magic_width, ":time cmd", 
+    printf(magic_format, indent, magic_width, ":time cmd",
            "time the given command");
-    printf(magic_format, indent, magic_width, ":version", 
+    printf(magic_format, indent, magic_width, ":version",
            "print version information");
-           
+
     printf("\nintrospection:\n");
-    printf("%splace a '?' after a variable or expression to show metadata\n", 
+    printf("%splace a '?' after a variable or expression to show metadata\n",
            indent);
   }
-  
+
   free(argument);
   return(plain_help);
 }
@@ -117,9 +117,9 @@ int ridl_magic_help(char *line, int firstcharIndex) {
 void ridl_magic_time(char *cmd) {
   clock_t clk0 = clock(), clk1;
   double seconds;
-  
+
   int error = ridl_executestr(cmd, 1);
-  
+
   clk1 = clock();
   seconds = (double) (clk1 - clk0) / (double) CLOCKS_PER_SEC;
 
@@ -140,14 +140,14 @@ void ridl_magic_printhistory(int numCmds, int showCmdnum) {
 
   hs = history_get_history_state();
   for (i = numCmds - 1; i >= 0; i--) {
-    h = history_get(hs->offset - i); 
+    h = history_get(hs->offset - i);
     if (showCmdnum) {
       printf("[%d]: %s\n", hs->offset - i, h == 0 ? "" : h->line);
     } else {
       printf("%s\n", h == 0 ? "" : h->line);
     }
   }
-} 
+}
 
 
 /**
@@ -167,7 +167,7 @@ void ridl_magic_history(char *line, int firstcharIndex) {
   
   if (strlen(first) > 0) {
     test = atoi(first);
-    if (test != 0) { 
+    if (test != 0) {
       numCmds = test;
     } else if (strcmp(first, "nonum") == 0) {
       showCmdnum = 0;
@@ -175,14 +175,14 @@ void ridl_magic_history(char *line, int firstcharIndex) {
       ridl_warning("unknown :history option '%s'", first);
       show = 0;
     }
-  }   
+  }
 
   if (show) {
     second = ridl_getnextword(line, firstcharIndex + length + search_length + 1, &search_length);
-    
+
     if (strlen(second) > 0) {
       test = atoi(second);
-      if (test != 0) { 
+      if (test != 0) {
         numCmds = test;
       } else if (strcmp(second, "nonum") == 0) {
         showCmdnum = 0;
@@ -192,11 +192,11 @@ void ridl_magic_history(char *line, int firstcharIndex) {
       }
     }
 
-    free(second);    
+    free(second);
   }
-  
-  free(first); 
-      
+
+  free(first);
+
   if (show) ridl_magic_printhistory(numCmds, showCmdnum);
 }
 
@@ -214,22 +214,22 @@ void ridl_magic_histedit(char *line, int firstcharIndex) {
   char *snlines = ridl_getnextword(line, firstcharIndex + 10, &search_length);
   char *filename = ridl_getnextword(line, firstcharIndex + 10 + search_length + 1, &search_length);
   int i, nlines = atoi(snlines);
-  
+
   if (ridl_file_exists(filename)) {
     ridl_warning("file '%s' already exists", filename);
     return;
   }
-  
+
   FILE *fp = fopen(filename, "w");
-  
+
   hs = history_get_history_state();
   for (i = nlines - 1; i >= 0; i--) {
-    h = history_get(hs->offset - i); 
+    h = history_get(hs->offset - i);
     fprintf(fp, "%s\n", h == 0 ? "" : h->line);
   }
   free(snlines);
   free(filename);
   fclose(fp);
   
-  ridl_launcheditor(filename); 
-}   
+  ridl_launcheditor(filename);
+}
